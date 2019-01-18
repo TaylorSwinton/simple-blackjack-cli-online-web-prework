7b5c7b4bc3b0f6ec4640b09acdd6d185a04102e0
Taylor Swinton <taylorswinton1@gmail.com>
2019-01-18 04:53:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,7 +53,7 @@ def runner
   card_total = initial_round
 
     until hit?(card_total) >= 21
-      sum = card_total + hit?()
+      card_total = card_total + hit?()
       display_card_total(card_total)
     end
   end_game(card_total)
